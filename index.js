c6884622dafe3de8a181748b0a1635932df5b027
Vi Vo <aaronvo118@gmail.com>
2021-12-15 16:23:49 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "V is the champion"
+
+
